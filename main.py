6bb215fede39fe9b090f5aa56a1ad909820db007
Lloyd <lloydmanzanedo@gmail.com>
2025-05-31 16:02:51 -0500

    Agrego archivos iniciales

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from prefect import flow
 from etl.extract import extract_jobs
-
+from etl.transform import transform_jobs
+from etl.load import load_jobs
 
 @flow
 def etl_pipeline():
     print("PIPELINE DE ETL LINKEDIN")
-    extract_jobs()
+    jobs = extract_jobs()
+    jobs_transformed = transform_jobs(jobs)
+    load_jobs(jobs_transformed)
     print("ETL COMPLETADO")
+    
 etl_pipeline()
